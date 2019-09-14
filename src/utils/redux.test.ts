e3204bc65dce58785ed55ef0e24887841fec0e9e
Will Chen <will@asianafrowill.com>
2019-09-14 15:41:50 -0700

    Have Redux util support creating an action with a payload.

--- src/utils/redux.test.ts
+++ src/utils/redux.test.ts
@@ -4,4 +4,12 @@ describe("redux", () => {
   it("generates an identity action", () => {
     expect(createAction("someAction")).toEqual({ type: "someAction" });
   });
+
+  it("generates an action with payload", () => {
+    const data = { some: "data" };
+    expect(createAction("someType", data)).toEqual({
+      type: "someType",
+      payload: data,
+    });
+  });
 });
