b5b4d0c44acdc51ae9c78127d81608ced549bd64
Will Chen <will@asianafrowill.com>
2019-09-14 15:41:50 -0700

    Have Redux util support creating an action with a payload.

--- src/utils/redux.ts
+++ src/utils/redux.ts
@@ -1,10 +1,17 @@
-import { Action } from "redux";
+import { Action as ReduxAction } from "redux";
 
+export interface Action<T, P = undefined> extends ReduxAction<T> {
+  payload: P | undefined;
+}
 /**
  * Create an action with type.
  * @param actionType
  * @returns Identity action of type @param actionType.
  */
-export const createAction = <T extends string>(actionType: T): Action<T> => ({
+export const createAction = <T extends string, P = any>(
+  actionType: T,
+  payload?: P,
+): Action<T, P> => ({
   type: actionType,
+  payload: payload || undefined,
 });
